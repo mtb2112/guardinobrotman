ca11554f87712734cdc818e48add2a3daaf99dda
ufnc <guardinopaul@gmail.com>
2014-05-13 09:18:44 -0400

    first commit

---
+++ assets/js/src/guardino_brotman_wedding.js
@@ -0,0 +1,13 @@
+/**
+ * Guardino Brotman Wedding
+ * http://wordpress.org/themes
+ *
+ * Copyright (c) 2014 ufnc
+ * Licensed under the GPLv2+ license.
+ */
+ 
+ ( function( window, undefined ) {
+	'use strict';
+
+
+ } )( this );
