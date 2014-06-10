ba7568f677f520afba48d1108a6b43ced19bc07b
ufnc <guardinopaul@gmail.com>
2014-06-10 15:21:20 -0400

    adding more responsive shizzz

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -2,7 +2,7 @@ $().ready(function() {
 	var date = $('.gbwedding-date');	
 
     date.arctext({
-    	radius: 2000,
+    	radius: 640,
     	dir: -1
     });
 });
--- footer.php
+++ footer.php
@@ -8,7 +8,8 @@
 </div>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/src/arctext.js" type="text/javascript"></script>
-<script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/src/scripts.js" type="text/javascript"></script>
 <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/src/fittext.js" type="text/javascript"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/src/scripts.js" type="text/javascript"></script>
+
 </body>
 </html>
